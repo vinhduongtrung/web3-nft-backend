5a37624b99250b8b3ea59125465d85ad32e1ecb7
murrayadrian <vinhduongtrung812@outlook.com>
2023-07-08 08:41:08 +0700

    ok

---
+++ src/test/java/com/java/web3/Web3BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.web3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Web3BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
