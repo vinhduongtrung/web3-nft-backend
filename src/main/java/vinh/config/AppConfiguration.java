f0486b0703a91c5a4896069a62d954ff5be9ab54
murrayadrian <vinhduongtrung812@outlook.com>
2023-07-15 21:23:06 +0700

    update jwt

---
+++ src/main/java/vinh/config/AppConfiguration.java
@@ -0,0 +1,58 @@
+package vinh.config;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@Configuration
+public class AppConfiguration {
+	
+	@Bean
+	public SecurityFilterChain appConfig(HttpSecurity http) throws Exception {
+		http.sessionManagement()
+		.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+		.and()
+		.authorizeHttpRequests()
+		.requestMatchers(HttpMethod.POST,"/api/auth/**").permitAll()
+		.requestMatchers(HttpMethod.GET,"/swagger-ui/**","/").permitAll()
+		.requestMatchers(HttpMethod.GET,"/ws/**").permitAll()
+		.anyRequest().authenticated()
+		.and()
+		.addFilterBefore(new JwtTokenVadidationFilter(), BasicAuthenticationFilter.class)
+		.csrf().disable()
+		.cors()
+		.configurationSource(new CorsConfigurationSource() {
+			
+			@Override
+			public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+				CorsConfiguration cfg = new CorsConfiguration();
+				cfg.setAllowedOrigins(Arrays.asList(
+					"http://localhost:5173"
+				));
+				cfg.setAllowedMethods(Collections.singletonList("*"));
+				cfg.setAllowCredentials(true);
+				cfg.setAllowedHeaders(Collections.singletonList("*"));
+				cfg.setExposedHeaders(Arrays.asList("Authorization"));
+				cfg.setMaxAge(3600L);
+				return cfg;
+			}
+		})
+		.and()
+		.httpBasic()
+		.and()
+		.formLogin();
+		
+		return http.build();
+	}
+}
